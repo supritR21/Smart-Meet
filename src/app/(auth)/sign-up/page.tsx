a8e8961e83512fb80126eb547d0cad58db254cd2
supritR21 <supritraj30@gmail.com>
2025-11-05 10:48:06 +0530

    Authentication UI

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const Page = () => {
+    console.log("Sign up");
+    return (
+        <SignUpView />
+    )
+}
+export default Page;
