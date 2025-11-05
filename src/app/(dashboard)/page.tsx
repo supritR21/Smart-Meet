f3f174733eaaba2ef73073ecb580ad24cdb290cb
supritR21 <supritraj30@gmail.com>
2025-11-05 23:56:31 +0530

    tRPC Setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,10 +3,13 @@ import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
+//import { HomeView } from "@/modules/home/ui/views/home-view";
+import {caller} from "@/trpc/server";
 
 // http://localhost:3000
 
 const Page = async () => {
+  //const data = await caller.hello({text: "Suprit Server"});
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -14,6 +17,8 @@ const Page = async () => {
   if(!session) {
     redirect("/sign-in");
   }
+
+  //return <p>{data.greeting}</p>
   return <HomeView />;
 };
 
