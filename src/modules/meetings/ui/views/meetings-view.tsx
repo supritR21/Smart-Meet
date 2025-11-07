40f6503d815adfc46df879f23ca2630b33f51fcf
supritR21 <supritraj30@gmail.com>
2025-11-07 15:45:13 +0530

    16 Meetings Setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {useTRPC} from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+
+export const MeetingsView = () => {
+    const trpc = useTRPC();
+    const {data} = useQuery(trpc.meetings.getMany.queryOptions({}));
+    return (
+        <div>
+            {JSON.stringify(data?.items)}
+        </div>
+    );
+};
+
+export const MeetingsViewLoading = () => {
+    return (
+        <LoadingState
+           title="Loading Meetings" description="This may take few seconds"
+        />
+    )
+}
+
+export const MeetingsViewError = () => {
+    return (
+        <ErrorState
+            title="Error Loading Meetings"
+            description="Something went wrong"
+        />
+    )
+}
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
 import {agentsRouter} from "@/modules/agents/server/procedures";
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
 import {createTRPCRouter} from "@/trpc/init";
 
 export const appRouter = createTRPCRouter({
     agents: agentsRouter,
+    meetings: meetingsRouter,
 });
 export type AppRouter = typeof appRouter;
